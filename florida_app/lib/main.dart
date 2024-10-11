0f010c34668f5b7281d8d26326649646a4ea6d9a
jha777 <jeffarrigui5@gmail.com>
2024-10-10 23:41:02 -0500

    directoriofloridapp1

--- florida_app/lib/main.dart
+++ florida_app/lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
